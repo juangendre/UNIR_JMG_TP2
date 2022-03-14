1e8345f038fd982c9198fcde17e80a6ccbaa6763
juangendre <juan.gendre@gmail.com>
2022-03-14 13:12:23 -0300

    network

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,5 @@
 # Creación de red
-resource "azurerm_virtual_network" "vnLmCp2" {
+resource "azurerm_virtual_network" "vnJMG" {
     name                = "${var.prefix}-netkubernetes"
     address_space       = ["192.168.0.0/16"]
     location            = azurerm_resource_group.rg.location
@@ -11,10 +11,10 @@ resource "azurerm_virtual_network" "vnLmCp2" {
 }
 
 # Creación de subnet
-resource "azurerm_subnet" "subNetLmCp2" {
+resource "azurerm_subnet" "subNetJMG" {
     name                   = "${var.prefix}-subnetterraform"
     resource_group_name    = azurerm_resource_group.rg.name
-    virtual_network_name   = azurerm_virtual_network.vnLmCp2.name
+    virtual_network_name   = azurerm_virtual_network.vnJMG.name
     address_prefixes       = ["192.168.1.0/24"]
 
 }
@@ -27,10 +27,10 @@ resource "azurerm_network_interface" "nic-master" {
 
     ip_configuration {
     name                           = "ipvmmaster"
-    subnet_id                      = azurerm_subnet.subNetLmCp2.id 
+    subnet_id                      = azurerm_subnet.subNetJMG.id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "192.168.1.110"
-    public_ip_address_id           = azurerm_public_ip.publicIpMasterLmCp2.id
+    public_ip_address_id           = azurerm_public_ip.publicIpMasterJMG.id
   }
 
     tags = {
@@ -40,7 +40,7 @@ resource "azurerm_network_interface" "nic-master" {
 }
 
 # IP pública - Master
-resource "azurerm_public_ip" "publicIpMasterLmCp2" {
+resource "azurerm_public_ip" "publicIpMasterJMG" {
   name                = "${var.prefix}-vmpublicipmaster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -61,10 +61,10 @@ resource "azurerm_network_interface" "nic-nfs" {
 
     ip_configuration {
     name                           = "ipvmnfs"
-    subnet_id                      = azurerm_subnet.subNetLmCp2.id 
+    subnet_id                      = azurerm_subnet.subNetJMG.id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "192.168.1.115"
-    public_ip_address_id           = azurerm_public_ip.publicIpNfsLmCp2.id
+    public_ip_address_id           = azurerm_public_ip.publicIpNfsJMG.id
   }
 
     tags = {
@@ -74,7 +74,7 @@ resource "azurerm_network_interface" "nic-nfs" {
 }
 
 # IP pública - NFS
-resource "azurerm_public_ip" "publicIpNfsLmCp2" {
+resource "azurerm_public_ip" "publicIpNfsJMG" {
   name                = "${var.prefix}-vmpublicipnfs"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -96,10 +96,10 @@ resource "azurerm_network_interface" "nic-worker-1" {
 
     ip_configuration {
     name                           = "ipvmworker1"
-    subnet_id                      = azurerm_subnet.subNetLmCp2.id 
+    subnet_id                      = azurerm_subnet.subNetJMG.id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "192.168.1.111"
-    public_ip_address_id           = azurerm_public_ip.publicIpWorker1LmCp2.id
+    public_ip_address_id           = azurerm_public_ip.publicIpWorker1JMG.id
   }
 
     tags = {
@@ -109,7 +109,7 @@ resource "azurerm_network_interface" "nic-worker-1" {
 }
 
 # IP pública - Worker1
-resource "azurerm_public_ip" "publicIpWorker1LmCp2" {
+resource "azurerm_public_ip" "publicIpWorker1JMG" {
   name                = "${var.prefix}-vmpublicipworker1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -130,10 +130,10 @@ resource "azurerm_network_interface" "nic-worker-2" {
 
     ip_configuration {
     name                           = "ipvmworker2"
-    subnet_id                      = azurerm_subnet.subNetLmCp2.id 
+    subnet_id                      = azurerm_subnet.subNetJMG.id 
     private_ip_address_allocation  = "Static"
     private_ip_address             = "192.168.1.112"
-    public_ip_address_id           = azurerm_public_ip.publicIpWorker2LmCp2.id
+    public_ip_address_id           = azurerm_public_ip.publicIpWorker2JMG.id
   }
 
     tags = {
@@ -143,7 +143,7 @@ resource "azurerm_network_interface" "nic-worker-2" {
 }
 
 # IP pública - Worker2
-resource "azurerm_public_ip" "publicIpWorker2LmCp2" {
+resource "azurerm_public_ip" "publicIpWorker2JMG" {
   name                = "${var.prefix}-vmpublicipworker2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
